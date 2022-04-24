9993907aa2a182fbaec5ea43700803d5d9a61953
Михаил <mikhailsivykh@yandex.ru>
2022-04-24 20:03:10 +0300

    Написал приветствие, функцию самой игры, дошёл до условий, обозначил функцию продолжения

---
+++ Bagels.py
@@ -0,0 +1,29 @@
+from random import randint
+
+
+def welcome():  # приветствие, правила
+    print('''I am thinking of a 3-digit number. Try to guess what it is.
+    Here are some clues:''')
+    print('''When I say: That means:
+Pico: One digit is correct but in the wrong position.
+Fermi: One digit is correct and in the right position.
+Bagels: No digit is correct.''')
+
+
+def game():  # собственно, сама игра
+    number = str(randint(100, 999))  # генерируем случайное число из диапазона (100, 999) и переводим в строку
+    print('I have thought a number!')
+    attempt = input('Write a number. Try to guess!')  # игрок вводит число
+    while not attempt.isdigit() and len(attempt) != 3:
+        attempt = input('Incorrect answer. Try again!')
+    if attempt == number:
+        print('You got it!')
+        continue_game()
+    for i in attempt:
+        if i in number:
+
+
+
+
+def continue_game():  # предложение сыграть ещё раз...
+    pass
