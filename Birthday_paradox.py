e5ffd7409a225ff2c6b6d0082a86560896bc6af0
Михаил <mikhailsivykh@yandex.ru>
2022-04-26 15:11:42 +0300

    Написал всё. Осталось только зациклить для приличия, но это в принципе необязательно.

--- Birthday_paradox.py
+++ Birthday_paradox.py
@@ -2,27 +2,31 @@ from datetime import date
 from random import randint
 
 
-# today = date.today()
-# print(today)
-
-# my_birthday = date(2001, 10, 3)
-# print(my_birthday)
-# print(my_birthday.fromordinal(my_birthday.toordinal()))
-# print(my_birthday.toordinal())
-# print(my_birthday.ctime())
-# my_set = my_birthday.ctime().split()
-# print(my_set)
-
 start = date(2022, 1, 1).toordinal()
 end = date(2022, 12, 31).toordinal()
+coin = 0
 
-random_date = date.fromordinal(randint(start, end))
-random_date = random_date.ctime()
-days = [date.fromordinal(randint(start, end)).ctime().split() for _ in range(23)]
-print(days)
-birthdays = list(map(lambda x: tuple(x[1:3]), days))
-print(birthdays)
-birthdays = set(birthdays)
-print(birthdays)
-if len(days) > len(birthdays):
-    print('Дни рождения совпали! Хотя бы два дня рождения пришлись на одну и ту же дату')
+
+print('''Давайте проведём эксперимент по имитационному моделированию! Люблю такое, я ж в определённом смысле 
+искусственный интеллект, чтоб меня. Давайте представим, что есть определённое количество человек, каждый из которых 
+называет дату своего рождения (месяц и день).
+Как думаете, какова вероятность того, что среди этих людей найдётся хотя бы пара человек, у которых день рождения
+совпадёт?
+Вам нужно будет ввести количество людей в предполагаемой группе, а также количество итераций, которое мне необходимо 
+сделать для вычисления вероятности. Помните, чем больше итераций, тем точнее результат!
+P.S однако слишком увлекаться не советую: я могу надолго зависнуть :)''')
+dates = int(input('Введите количество человек в группе'))
+n = int(input('Введите количество итераций'))
+for i in range(n):
+    random_date = date.fromordinal(randint(start, end))
+    random_date = random_date.ctime()
+    days = [date.fromordinal(randint(start, end)).ctime().split() for _ in range(dates)]
+    birthdays = list(map(lambda x: tuple(x[1:3]), days))
+    birthdays = set(birthdays)
+    if len(days) > len(birthdays):
+        coin += 1
+    if i % 10000 == 0:
+        print(f'Работаю. Выполнено итераций: {i}...')
+percent = coin / n * 100
+print(f'За {n} итераций мы получили {coin} совпадений (что хотя бы два человека из {dates} родились в одну и ту же дату)')
+print(f'Вероятность при основании подсчёта {dates} равна {percent}%')
