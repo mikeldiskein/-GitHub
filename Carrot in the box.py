8c807e90d65ebff77c358457f7a373fff5545b62
Михаил <mikhailsivykh@yandex.ru>
2022-05-27 16:34:50 +0300

    Just fixed, all is working

--- Carrot in the box.py
+++ Carrot in the box.py
@@ -12,8 +12,7 @@ def continue_game():
         print('See you again!')
 
 
-
-def game():
+def game():  # game itself :)
     var = (1, 2)
     box_1 = []
     box_2 = []
@@ -23,7 +22,7 @@ def game():
         box_1.append('carrot')
     else:
         box_2.append('carrot')
-    print(player_1, ', are you ready to look in your box? yes or no')
+    print(f'{player_1}, are you ready to look in your box? yes or no')
     while True:
         answer = input()
         if answer == 'yes':
@@ -33,13 +32,13 @@ def game():
             print('Then think a little else')
         else:
             print('Write please yes or no')
-    print(player_1, ', is there the carrot in your box?')
+    print(f'{player_1}, is there the carrot in your box?')
     print(input())
-    print(player_2, ', do you want to exchange your box with ', player_1, '?')
+    print(f'{player_2}, do you want to exchange your box with {player_2}?')
     while True:
         answer = input()
         if answer == 'yes':
-            print(f'Then your box is {box_1}, and the {player_1} box is {box_2}')
+            print(f'Then your box is box_1, and the {player_1} box is box_2')
             players[player_1], players[player_2] = box_2, box_1
             break
         elif answer == 'no':
@@ -48,9 +47,12 @@ def game():
         else:
             print('Write please yes or no')
     print('So, now we will know in which box the carrot is...')
-    print(players[player_1])
-    print(players[player_2])
-
+    print(f'{player_1}:', players[player_1])
+    print(f'{player_2}:', players[player_2])
+    if len(players[player_1]) > 0:
+        print(f'{player_1} won!')
+    elif len(players[player_2]) > 0:
+        print(f'{player_2} won!')
 
 
 # hello
@@ -60,3 +62,5 @@ print('First player, write your name')
 player_1 = input()
 print('Second player, write your name')
 player_2 = input()
+
+game()
